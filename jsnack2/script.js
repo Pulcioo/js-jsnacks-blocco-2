d104ac817ae661eea2bdf9d08a466670cc4a0e07
Davide Pulci <davide.pulci@gmail.com>
2022-03-11 19:35:08 +0100

    jsnack2 completato

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,9 +5,10 @@ console.log("JS OK")
 // Esegui questo programma in due versioni, con il for e con il while.
 
 // chiedere per 5 volte all'utente di inserire un numero
-// FOR
 let somma = 0
 
+// FOR
+/*
 for (let i = 0; i < 5; i++) {
     // chiedo all'utente di inserire un numero
     let numeroUtente = prompt('Inserisci un numero')
@@ -20,7 +21,23 @@ for (let i = 0; i < 5; i++) {
     // calcolo la somma 
     somma = somma + parseInt(numeroUtente)
 }
+*/
 
+// WHILE
+let i = 0;
+while (i < 5) {
+    // chiedo all'utente di inserire un numero
+    let numeroUtente = prompt('Inserisci un numero')
+    // fintanto che l'utente non mette un numero richiedo di inserire nuovamente
+    while (isNaN(numeroUtente)) {
+        numeroUtente = parseInt(prompt('Hai sbagliato a digitare...inserisci un numero'))
+    }
+    // stampo numeri inseriti
+    console.log(numeroUtente)
+    // calcolo la somma 
+    somma = somma + parseInt(numeroUtente)
+    i++
+}
 
 console.log(`La somma dei numeri da te inseriti è: ${somma}`)
 
